af241d6688c672dda741e8ae6af07ec922faf89f
Charles Burch <georgia_beast89@yahoo.com>
2018-12-17 12:27:03 -0600

    changed home route to server shop.html file

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const path = require('path')
+
 // this is the default route
 router.get('/',(req,res,next)=> {
-    res.send('<h1>Hello world this is a test</h1>');
+    res.sendFile(path.join(__dirname, '../','views', 'shop.html'));
 })
 
 module.exports = router;
