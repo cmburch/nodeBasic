959979814e93f81f8e215b34136e8aa426fd2381
Charles Burch <georgia_beast89@yahoo.com>
2018-12-17 10:03:56 -0600

    added body-parser and created fake product route just for testing purpose

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product',(req,res,next)=>{
     console.log('inside of the middleware');
-    res.send('<h1>Products Page Test</h1>');
+    res.send('<form action="/product" method="POST" ><input type="text" name="title"><button type="submit">Add Product</button></form>');
     // next(); //allows the request to continue to the next middleware in line
 })
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
 
+//this is the default route
 app.use('/',(req,res,next)=>{
     res.send('<h1>Hello world this is a test</h1>');
 })
@@ -16,3 +23,4 @@ app.use('/',(req,res,next)=>{
 // const server = http.createServer(app);
 
 app.listen(3000);
+
