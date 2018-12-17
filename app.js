9e72e17d34386ec74998d46d533f281aff9e2a4d
Charles Burch <georgia_beast89@yahoo.com>
2018-12-17 08:37:43 -0600

    added express and added simple middleware to see if everything is working

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require('http');
 
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('inside of the middleware')
+    next(); //allows the request to continue to the next middleware in line
+})
+
+app.use((req,res,next)=>{
+    res.send('<h1>Hello world this is a test</h1>');
+})
+
 //this return a server
-const server = http.createServer((req,res) => {
-    console.log(req);  
-});
+const server = http.createServer(app);
 
 server.listen(3000);
