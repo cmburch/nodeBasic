508c8bd71b7946db4b2c2657cbe752dae89c8471
Charles Burch <georgia_beast89@yahoo.com>
2018-12-17 09:03:29 -0600

    testing out the routes to check if the routes send response

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('inside of the middleware')
-    next(); //allows the request to continue to the next middleware in line
+app.use('/add-product',(req,res,next)=>{
+    console.log('inside of the middleware');
+    res.send('<h1>Products Page Test</h1>');
+    // next(); //allows the request to continue to the next middleware in line
 })
 
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello world this is a test</h1>');
 })
 
 //this return a server
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
