93ae4c11e8c1efe2b95cc87064a024ec64a0ddbd
Charles Burch <georgia_beast89@yahoo.com>
2018-12-17 11:47:30 -0600

    routes misspelling fixed

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const adminRoutes = require('./routes/admin')
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+//error page if no route is found
+app.use((req,res,next)=>{
+    res.status(404).send('<h1> Page not Found</h1>');
+});
 
 //this return a server
 // const server = http.createServer(app);
